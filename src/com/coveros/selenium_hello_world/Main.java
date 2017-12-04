99682e0a6074a4d43ca0f2a1f715e97bdab99894
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-12-04 10:24:56 -0500

    changed capability back

--- src/com/coveros/selenium_hello_world/Main.java
+++ src/com/coveros/selenium_hello_world/Main.java
@@ -48,7 +48,7 @@ public class Main {
 		catch(IOException ex){
 			System.out.println("Error reading file "+ fileName+"");
 		}
-		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		DesiredCapabilities capability = DesiredCapabilities.firefox();
 		WebDriver driver = new RemoteWebDriver(new URL("http://172.31.2.108:4444/wd/hub"),capability);
 		
 		driver.get("http://"+DNS+":8080/hello-world-"+appVersion+"/");
